a73d6a7e1876c944ea30d1b68bb82577c50f028c
takashimakazuki <sora3takashima@gmail.com>
2022-10-16 03:13:47 +0900

    add ShapeList

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 https://zenn.dev/mebiusbox/books/8d9c42883df9f6/viewer/b85221
 
 - theta: the angle of field of view
-- 
--- rayt.h
+++ rayt.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <random>
 #include <float.h> // FLT_MIN, FLT_MAX
+#include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -20,6 +21,8 @@ typedef Vector3 col3;
 #define LOG2 1.442695f
 #define EPSILON 1e-6f
 #define GAMMA_FACTOR 2.2f
+
+#define NUM_THREAD 8
 
 inline float pow2(float x)
 {
@@ -47,6 +50,10 @@ inline float degrees(float rad) { return (rad / PI) * 180.f; }
 
 namespace rayt
 {
+
+    class Shape;
+    typedef std::shared_ptr<Shape> ShapePtr;
+
     class Image
     {
     public:
@@ -141,6 +148,95 @@ namespace rayt
         vec3 m_uvw[3]; // orthonormal basis vector
     };
 
+    class HitRec
+    {
+    public:
+        float t;
+        vec3 p;
+        vec3 n;
+    };
+
+    class Shape
+    {
+    public:
+        virtual bool hit(const Ray &r, float t0, float t1, HitRec &hrec) const = 0;
+    };
+
+    class Sphere : public Shape
+    {
+    public:
+        Sphere() {}
+        Sphere(const vec3 &c, float r)
+            : m_center(c),
+              m_radius(r) {}
+
+        virtual bool hit(const Ray &r, float t0, float t1, HitRec &hrec) const override
+        {
+            vec3 oc = r.origin() - m_center;
+            float a = dot(r.direction(), r.direction());
+            float b = 2.0 * dot(oc, r.direction());
+            float c = dot(oc, oc) - pow2(m_radius);
+            float D = b * b - 4 * a * c;
+            if (D > 0)
+            {
+                float root = sqrtf(D);
+                float temp = (-b - root) / (2.0f * a);
+                if (temp < t1 && temp > t0)
+                {
+                    hrec.t = temp;
+                    hrec.p = r.at(hrec.t);
+                    hrec.n = (hrec.p - m_center) / m_radius;
+                    return true;
+                }
+                temp = (-b + root) / (2.0f * a);
+                if (temp < t1 && temp > t0)
+                {
+                    hrec.t = temp;
+                    hrec.p = r.at(hrec.t);
+                    hrec.n = (hrec.p - m_center) / m_radius;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    private:
+        vec3 m_center;
+        float m_radius;
+    };
+
+    class ShapeList : public Shape
+    {
+    public:
+        ShapeList() {}
+
+        void add(const ShapePtr &shape)
+        {
+            m_list.push_back(shape);
+        }
+
+        virtual bool hit(const Ray &r, float t0, float t1, HitRec &hrec) const override
+        {
+            HitRec temp_rec;
+            bool hit_anything = false;
+            float closest_so_far = t1;
+            for (auto &p : m_list)
+            {
+                if (p->hit(r, t0, closest_so_far, temp_rec))
+                {
+                    hit_anything = true;
+                    closest_so_far = temp_rec.t;
+                    hrec = temp_rec;
+                }
+            }
+            return hit_anything;
+        }
+
+    private:
+        std::vector<ShapePtr> m_list;
+    };
+
     class Scene
     {
     public:
@@ -151,39 +247,25 @@ namespace rayt
 
         void build()
         {
+            // Camera
             vec3 w(-2.0f, -1.0f, -1.0f);
             vec3 u(4.0f, 0.0f, 0.0f);
             vec3 v(0.0f, 2.0f, 0.0f);
             m_camera = std::unique_ptr<Camera>(new Camera(u, v, w));
+
+            // Shapes
+            ShapeList *world = new ShapeList();
+            world->add(std::make_shared<Sphere>(vec3(0, 0, -1), 0.5f));
+            world->add(std::make_shared<Sphere>(vec3(0, -100.5, -1), 100));
+            m_world.reset(world);
         }
 
-        float hit_sphere(const vec3 &center, float radius, const Ray &r) const
+        vec3 color(const rayt::Ray &r, const Shape *world) const
         {
-            vec3 oc = r.origin() - center;
-            float a = dot(r.direction(), r.direction());
-            float b = 2.0f * dot(r.direction(), oc);
-            float c = dot(oc, oc) - pow2(radius);
-            // Discriminant that distinguish if the simultaneous equation between ray equation and sphere equation has 2 answers.
-            // → conflict detection with ray and sphere
-            float D = b * b - 4 * a * c;
-            if (D < 0)
+            HitRec hrec;
+            if (world->hit(r, 0, FLT_MAX, hrec))
             {
-                return -1.0f;
-            }
-            else
-            {
-                return (-b - sqrtf(D)) / (2.0f * a);
-            }
-        }
-
-        vec3 color(const Ray &r)
-        {
-            vec3 c(0, 0, -1);
-            float t = hit_sphere(c, 0.5f, r);
-            if (t > 0.0f)
-            {
-                vec3 N = normalize(r.at(t) - c);
-                return 0.5f * (N + vec3(1.0f));
+                return 0.5f * (hrec.n + vec3(1.0f));
             }
             return backgroundSky(r.direction());
         }
@@ -200,6 +282,7 @@ namespace rayt
             return lerp(t, vec3(1), vec3(0.5f, 0.7f, 1.0f));
         }
 
+#pragma omp parallel for schedule(dynamic, 1) num_threads(NUM_THREAD)
         void render()
         {
 
@@ -216,7 +299,8 @@ namespace rayt
                     float u = float(i + drand48()) / float(nx);
                     float v = float(j + drand48()) / float(ny);
                     Ray r = m_camera->getRay(u, v);
-                    vec3 c = color(r);
+                    vec3 c = color(r, m_world.get());
+
                     m_image->write(i, (ny - j - 1), c.getX(), c.getY(), c.getZ());
                 }
             }
@@ -227,6 +311,8 @@ namespace rayt
     private:
         std::unique_ptr<Camera> m_camera;
         std::unique_ptr<Image> m_image;
+        std::unique_ptr<Shape> m_world;
         vec3 m_backColor;
     };
+
 };
